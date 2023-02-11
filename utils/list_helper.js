d9cf47072853fa8a4e5e1b7ceca341e6fc0e0e28
Sergio <salliana@technisys.com>
2023-02-11 20:22:40 -0300

    Testing Express servers, user administration: Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return blogs.length + 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes
 };
