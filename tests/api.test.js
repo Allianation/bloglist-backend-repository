04a8f1ca53ff356a389a96350dc4ee21150b0cd0
Sergio <salliana@technisys.com>
2023-02-12 03:17:49 -0300

    Testing Express servers, user administration: Exercise 4.8

---
+++ tests/api.test.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+const initialNotes = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialNotes[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialNotes[1]);
+  await blogObject.save();
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("the first blog is about React patterns", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body[0].title).toBe("React patterns");
+});
+
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test("a specific blog is within the returned blogs", async () => {
+  const response = await api.get("/api/blogs");
+
+  const contents = response.body.map((r) => r.title);
+  expect(contents).toContain("React patterns");
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
