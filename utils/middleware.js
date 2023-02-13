aaef929f5ce9d51e01b8bc7e6bb512a2effcc65f
Sergio <salliana@technisys.com>
2023-02-13 02:39:16 -0300

    Testing Express servers, user administration: Exercise 4.19

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,7 +1,11 @@
 const logger = require("./logger");
 
 const requestLogger = (request, response, next) => {
-  logger.info(`Method: ${request.method} - Path: ${request.path} - Body: ${JSON.stringify(request.body)}`);
+  logger.info(
+    `Method: ${request.method} - Path: ${request.path} - Body: ${JSON.stringify(
+      request.body
+    )}`
+  );
   next();
 };
 
@@ -16,6 +20,10 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({
+      error: "invalid token",
+    });
   }
 
   next(error);
